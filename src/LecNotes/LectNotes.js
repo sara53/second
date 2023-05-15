0a67b87ef6084267751021bffbedca077c7e4993
sara <sara.atef001@gmail.com>
2023-05-15 18:01:59 +0300

    day03 demo

---
+++ src/LecNotes/LectNotes.js
@@ -0,0 +1,26 @@
+/**
+ *  == Lib VS Framework .
+ *  == what is react .
+ *  == SPA  VS MPA .
+ *  == what is JSX ?
+ *  == dealing with style in react
+ *  == working with expreesion
+ *  == add bootstrap , react bootstrap
+ *  == diff class componenent VS Function Component
+ *  == stateless Component VS Statefull compoent
+ *  == fire Event on react
+ *  == what is react props
+ *  == working state
+ *  == render list in react
+ *  == virtual dom VS Real Dom
+ *  == Render Element Conditionally
+ *  == component life cycle
+ *  == react Hooks
+ *  == use State , use Effect
+ *  == compoenet interaction
+ *      -- pass data from parent to child
+ *      -- pass data from child to parents
+ *
+ *
+ *
+ */
